e84875118fef12c80ca03b84c8004a543867a6df
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-01-28 21:57:52 -0300

    feat: initialize `package.json` with author, scripts, and main entry

--- internal/dependencies/init-package.go
+++ internal/dependencies/init-package.go
@@ -1,8 +1,10 @@
 package dependencies
 
 import (
+	"encoding/json"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func initPackageJSON() error {
@@ -20,6 +22,74 @@ func initPackageJSON() error {
 		return err
 	}
 
-	return nil
+	cmd = exec.Command("whoami")
 
+	out, err := cmd.Output()
+
+	if err != nil {
+		return err
+	}
+
+	cmd = exec.Command("touch", ".env")
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	person := strings.TrimSpace(string(out))
+
+	f, err := os.ReadFile("package.json")
+
+	if err != nil {
+		return err
+	}
+
+	var decodedJSON map[string]any
+
+	if err := json.Unmarshal(f, &decodedJSON); err != nil {
+		return err
+	}
+
+	for k := range decodedJSON {
+
+		switch k {
+		case "main":
+			{
+				decodedJSON[k] = "server.ts"
+			}
+		case "author":
+			{
+				decodedJSON[k] = person
+			}
+		case "scripts":
+			{
+				if scripts, ok := decodedJSON[k].(map[string]any); ok {
+					scripts["dev"] = "tsx watch --env-file=.env src/server.ts"
+					scripts["start"] = "node --env-file=.env build/server.js"
+					scripts["build"] = "tsdown src"
+					scripts["test"] = "vistest run"
+					scripts["test:watch"] = "vistest"
+					scripts["test:coverage"] = "vitest run --coverage"
+				}
+			}
+		default:
+			{
+				continue
+			}
+		}
+	}
+
+	encodedJSON, err := json.MarshalIndent(decodedJSON, "", "  ")
+
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("package.json", encodedJSON, os.ModePerm)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
