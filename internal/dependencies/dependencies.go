9d5cb51ec1ab93613053771a9ff45e24e99b0c97
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-01-29 21:11:46 -0300

    feat: create `src` and `src/env` directories with `index.ts`

--- internal/dependencies/dependencies.go
+++ internal/dependencies/dependencies.go
@@ -13,7 +13,6 @@ func installDependencies() error {
 		"bcryptjs",
 		"fastify",
 		"fastify-multer",
-		"multer",
 		"tsdown",
 		"zod",
 	}
---
+++ internal/src/main.go
@@ -0,0 +1,22 @@
+package src
+
+import (
+	"fmt"
+	"os"
+
+	"gitbhub.com/eduardongomes/ts-edu/internal/src/env"
+)
+
+func CreateSRCDIR() error {
+	dir := "src"
+
+	if err := os.Mkdir(dir, os.ModePerm); err != nil {
+		return fmt.Errorf("Failed to create directory src: %w", err)
+	}
+
+	if err := env.CreateEnvDIR(dir); err != nil {
+		return fmt.Errorf("Failed to create dir env src: %w", err)
+	}
+
+	return nil
+}
